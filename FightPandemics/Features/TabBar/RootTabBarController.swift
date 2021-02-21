cf872f30bf1bae89ef6db3dff0513fef0cb42504
Fernando Olivares <fernando.olivares@me.com>
2021-02-21 00:36:03 -0600

    #25: Move `addDotToTab(tab:)`
    Keep moving more functionality.

--- FightPandemics/Features/TabBar/RootTabBarController.swift
+++ FightPandemics/Features/TabBar/RootTabBarController.swift
@@ -138,51 +138,6 @@ final class RootTabBarController: UITabBarController {
         item?.titlePositionAdjustment = UIOffset(horizontal: horizontalOffset, vertical: 0)
     }
 
-    private func addDotToTab(tab: Tab) {
-        guard let item = tabBarItem(tab), let dotView = tabDotView(tab: tab) else {
-            return
-        }
-        let dotViewIdentifier = 100
-        if tabBar.viewWithTag(dotViewIdentifier) != nil {
-            tabBar.viewWithTag(dotViewIdentifier)?.removeFromSuperview()
-        }
-        dotView.tag = dotViewIdentifier
-        tabBar.addSubview(dotView)
-        tabBar.layoutIfNeeded()
-        tabBar.layoutSubviews()
-        item.setTitleTextAttributes([NSAttributedString.Key.foregroundColor: UIColor.clear],
-                                    for: .selected)
-    }
-
-    private func tabDotView(tab: Tab) -> UIView? {
-        guard let item = tabBarItem(tab), let itemView = item.value(forKey: "view") as? UIView else {
-            return nil
-        }
-
-        var addend = CGFloat()
-        switch tab {
-        case .feed:
-            addend = -19
-        case .search:
-            addend = -35
-        case .inbox:
-            addend = 27
-        case .profile:
-            addend = 12
-        case .post:
-            break
-        }
-
-        let dotView = UIView()
-        dotView.backgroundColor = UIColor.fightPandemicsNeonBlue()
-        dotView.frame.size = CGSize(width: 6, height: 6)
-        dotView.layer.cornerRadius = 3
-        dotView.layer.masksToBounds = true
-        dotView.frame.origin.x = itemView.frame.origin.x + (itemView.frame.size.width / 2) + addend
-        dotView.frame.origin.y = 40
-        return dotView
-    }
-
     @objc private func selectPostTab() {
         navigator.navigateToCreatePostEntitySelectionModal()
     }
